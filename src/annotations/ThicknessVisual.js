c3f6f36dcc1633101c221c9005515f29e854df1f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-05-26 12:39:52 +0200

    - extracted ThicknessVisual utility method to utils module

--- src/annotations/ThicknessVisual.js
+++ src/annotations/ThicknessVisual.js
@@ -4,62 +4,7 @@ import CrossHelper from "../CrossHelper"
 import SizeHelper from "../dimensions/SizeHelper"
 import BaseHelper from "../BaseHelper"
 
-
-
-export function getEntryExitThickness(entryInteresect, normalType="face"){
-  let normal  = entryInteresect.face.normal.clone()
-  switch(normalType)
-  {
-    case "face":
-    break
-    case "x":
-      normal = new THREE.Vector3(1,0,0)
-    break
-    case "y":
-      normal = new THREE.Vector3(0,1,0)
-    break
-    case "z":
-      normal = new THREE.Vector3(0,0,1)
-    break
-  }
-    
-  let object = entryInteresect.object
-  if( !object ) return undefined
-    
-  let entryPoint = entryInteresect.point.clone()
-  let flippedNormal = normal.clone().negate()
-  let offsetPoint = entryPoint.clone().add( flippedNormal.clone().multiplyScalar(10000))
-    
-  //get escape entryPoint
-  let raycaster  = new THREE.Raycaster(offsetPoint, normal.clone().normalize())
-  let intersects = raycaster.intersectObjects([object], true)
-    
-  let exitPoint = null
-  let minDist   = Infinity
-  
-  intersects.map(function(entry){
-      let curPt = entry.point
-      let curLn = curPt.clone().sub( entryPoint ).length()
-      
-      if( curLn < minDist )
-      {
-        exitPoint = curPt
-        minDist = curLn
-      }
-    })
-   
-  //FIXME: todo or not ??
-  object.worldToLocal( entryPoint )
-  object.worldToLocal( exitPoint )
-  
-  //compute actual thickness
-  let endToStart = exitPoint.clone().sub( entryPoint )
-  let thickness = endToStart.length()
-  
-  return {object: object, entryPoint: entryPoint, exitPoint:exitPoint, thickness:thickness}
-}
-
-
+import {getEntryExitThickness} from './utils'
 
 class ThicknessVisual extends AnnotationHelper {
   constructor( options ) {
--- src/annotations/utils.js
+++ src/annotations/utils.js
@@ -33,7 +33,6 @@ export function getTargetBoundsData( targetObject, point ){
 }   
 
 
-
 //compute center , dia/radius from three 3d points
 export function computeCenterDiaNormalFromThreePoints(pointA,pointB,pointC){
 
@@ -68,4 +67,57 @@ export function computeCenterDiaNormalFromThreePoints(pointA,pointB,pointC){
   let normal   = plane.normal
 
   return {center,diameter,normal}
+}
+
+export function getEntryExitThickness(entryInteresect, normalType="face"){
+  let normal  = entryInteresect.face.normal.clone()
+  switch(normalType)
+  {
+    case "face":
+    break
+    case "x":
+      normal = new THREE.Vector3(1,0,0)
+    break
+    case "y":
+      normal = new THREE.Vector3(0,1,0)
+    break
+    case "z":
+      normal = new THREE.Vector3(0,0,1)
+    break
+  }
+    
+  let object = entryInteresect.object
+  if( !object ) return undefined
+    
+  let entryPoint = entryInteresect.point.clone()
+  let flippedNormal = normal.clone().negate()
+  let offsetPoint = entryPoint.clone().add( flippedNormal.clone().multiplyScalar(10000))
+    
+  //get escape entryPoint
+  let raycaster  = new THREE.Raycaster(offsetPoint, normal.clone().normalize())
+  let intersects = raycaster.intersectObjects([object], true)
+    
+  let exitPoint = null
+  let minDist   = Infinity
+  
+  intersects.map(function(entry){
+      let curPt = entry.point
+      let curLn = curPt.clone().sub( entryPoint ).length()
+      
+      if( curLn < minDist )
+      {
+        exitPoint = curPt
+        minDist = curLn
+      }
+    })
+   
+  //FIXME: todo or not ??
+  object.worldToLocal( entryPoint )
+  object.worldToLocal( exitPoint )
+  
+  //compute actual thickness
+  let endToStart = exitPoint.clone().sub( entryPoint )
+  let thickness = endToStart.length()
+  
+  return {object: object, entryPoint: entryPoint, exitPoint:exitPoint, thickness:thickness}
 }
