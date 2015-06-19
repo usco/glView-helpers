02a426d9f388d4791430e77e72e443b674209611
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-06-19 13:15:06 +0200

    - modified centerMesh to default centering on z axis to false - semicolumn removal

--- src/meshTools/centerMesh.js
+++ src/meshTools/centerMesh.js
@@ -1,43 +1,43 @@
 import THREE from "three"
 import {computeBoundingSphere} from './computeBounds'
 
-var centerMesh=function( object, onX, onY, onZ )
+let centerMesh=function( object, onX, onY, onZ )
 {
   //TODO: should this be added to our object/mesh classes
-  var onX = onX === undefined ? false: onX;
-  var onY = onY === undefined ? false: onY;
-  var onZ = onZ === undefined ? true: onZ;
+  let onX = onX === undefined ? false: onX
+  let onY = onY === undefined ? false: onY
+  let onZ = onZ === undefined ? false: onZ
   
   //centering hack
-  if(!object.boundingSphere) computeBoundingSphere( object );
-  var offset = object.boundingSphere.center;
+  if(!object.boundingSphere) computeBoundingSphere( object )
+  let offset = object.boundingSphere.center
   
   object.traverse(function(item)
   {
     if(item.geometry){
-      item.geometry.applyMatrix( new THREE.Matrix4().makeTranslation( -offset.x, -offset.y, -offset.z ) );
+      item.geometry.applyMatrix( new THREE.Matrix4().makeTranslation( -offset.x, -offset.y, -offset.z ) )
     }
-  });
+  })
   
   //offset to move the object above given planes
   if(onZ)
   {
-    var h = object.boundingBox.max.z  - object.boundingBox.min.z ;
-    object.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0, h/2 ) );
+    let h = object.boundingBox.max.z  - object.boundingBox.min.z 
+    object.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0, h/2 ) )
   }
   
   if(onY)
   {
-    var d = object.boundingBox.max.y  - object.boundingBox.min.y ;
-    object.applyMatrix( new THREE.Matrix4().makeTranslation( 0, d/2, 0 ) );
+    let d = object.boundingBox.max.y  - object.boundingBox.min.y 
+    object.applyMatrix( new THREE.Matrix4().makeTranslation( 0, d/2, 0 ) )
   }
   
   if(onX)
   {
-    var w = object.boundingBox.max.x  - object.boundingBox.min.x ;
-    object.applyMatrix( new THREE.Matrix4().makeTranslation( w/2, 0, 0 ) );
+    let w = object.boundingBox.max.x  - object.boundingBox.min.x 
+    object.applyMatrix( new THREE.Matrix4().makeTranslation( w/2, 0, 0 ) )
   }
-  return object;
+  return object
 }
 
-export default centerMesh ;
+export default centerMesh 
