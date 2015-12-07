5012fa7ab061bb00b9e2ffcb85965863729dcceb
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-12-07 09:21:18 +0100

    fix(): fixed bad exports of annotations/measurements

--- src/annotations/annotations.js
+++
@@ -1,8 +0,0 @@
-
-exports.AnnotationVisual = require("./AnnotationVisual.js" );
-exports.DistanceVisual   = require("./DistanceVisual.js" );
-exports.NoteVisual       = require("./NoteVisual.js" );
-exports.DiameterVisual   = require("./DiameterVisual.js" );
-exports.AngleVisual     = require("./AngleVisual.js" );
-exports.ThicknessVisual  = require("./ThicknessVisual.js" );
-
--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import * as grids from './Grids'
 import * as planes from './Planes'
 import CamViewControls from './controls/CamViewControls2'
 //import * as objectEffects from './objectEffects'
-import * as annotations from './annotations/annotations'
 
+import AnnotationVisual from './annotations/AnnotationVisual'
+import DistanceVisual   from './annotations/DistanceVisual'
+import NoteVisual       from './annotations/NoteVisual.js'
+import DiameterVisual   from './annotations//DiameterVisual.js'
+import AngleVisual      from './annotations//AngleVisual.js'
+import ThicknessVisual  from './annotations//ThicknessVisual.js'
+
+const annotations = {AnnotationVisual, DistanceVisual, NoteVisual, DiameterVisual, AngleVisual, ThicknessVisual}
 
 import ZoomInOnObject from './objectEffects/zoomInOnObject'
 const objectEffects = {ZoomInOnObject}
