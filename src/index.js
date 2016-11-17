16e169666ab71b0709cea6c52b4db257c1188ec6
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-17 15:10:06 +0100

    refactor(cleanup): minor cleanups in main

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import * as grids from './Grids'
 import * as planes from './Planes'
 import CamViewControls from './controls/CamViewControls2'
-//import * as objectEffects from './objectEffects'
+// import * as objectEffects from './objectEffects'
 
 import AnnotationVisual from './annotations/AnnotationVisual'
-import DistanceVisual   from './annotations/DistanceVisual'
-import NoteVisual       from './annotations/NoteVisual.js'
-import DiameterVisual   from './annotations//DiameterVisual.js'
-import AngleVisual      from './annotations//AngleVisual.js'
-import ThicknessVisual  from './annotations//ThicknessVisual.js'
+import DistanceVisual from './annotations/DistanceVisual'
+import NoteVisual from './annotations/NoteVisual.js'
+import DiameterVisual from './annotations//DiameterVisual.js'
+import AngleVisual from './annotations//AngleVisual.js'
+import ThicknessVisual from './annotations//ThicknessVisual.js'
 
 const annotations = {AnnotationVisual, DistanceVisual, NoteVisual, DiameterVisual, AngleVisual, ThicknessVisual}
 
-//import ZoomInOnObject from './objectEffects/zoomInOnObject'
+// import ZoomInOnObject from './objectEffects/zoomInOnObject'
 
 import zoomInOn from './cameraEffects/zoomInOn'
 import zoomToFit from './cameraEffects/zoomToFit'
@@ -22,5 +22,4 @@ const cameraEffects = {zoomInOn, zoomToFit}
 import centerMesh from './meshTools/centerMesh'
 const meshTools = {centerMesh}
 
-export {grids, planes, meshTools,CamViewControls, cameraEffects, annotations}
-
+export { grids, planes, meshTools, CamViewControls, cameraEffects, annotations }
